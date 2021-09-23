08bc32229932f34ee3b73fc45e834558b040d427
Denis Khashchuk <khashchuk.denis@gmail.com>
2021-09-23 17:24:06 +0300

    should be fixed

--- 2lab/taskB/main.cpp
+++ 2lab/taskB/main.cpp
@@ -3,22 +3,45 @@
 
 using namespace std;
 
+int findIfInArray(string temp[], int size, string basicString) {
+    for(int i = 0;  i < size; i ++)
+        if(temp[i] == basicString)
+            return 1;
+    return 0;
+}
+
 int main() {
    ifstream fileIn("race.in");
    ofstream fileOut("race.out");
+
    int amount;
    fileIn >> amount;
-   int size = amount * 2;
-   string array[size];
-   for(int i =0; i < size; i++){
-        fileIn >> array[i];
+
+   string country[amount];
+   string surname[amount];
+
+   for(int i =0; i < amount; i++){
+        fileIn >> country[i] >> surname[i];
    }
-   
 
-
-    for(int i =0; i < size; i++){
-        fileOut << array[i] << " ";
-    }
+   for(int i = 0; i < amount; i ++){
+       for(int j = 0 ; j < amount; j++){
+           if(country[i]<country[j]){
+               swap(country[j],country[i]);
+               swap(surname[j],surname[i]);
+           }
+       }
+   }
+string tempArray[amount];
+   for(int i =0; i < amount; i++){
+       if(findIfInArray(tempArray, amount, country[i])){
+           fileOut << surname[i]<< "\n";
+       } else{
+           fileOut <<"=== " <<country[i]<<" ===\n";
+           tempArray[i] = country[i];
+           fileOut << surname[i] << "\n";
+       }
+   }
 
    fileIn.close();
    fileOut.close();
