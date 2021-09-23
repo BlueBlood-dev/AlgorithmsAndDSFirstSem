b9dd61e89eedcd9c79c91737aa5d5984ae4b3775
Denis Khashchuk <khashchuk.denis@gmail.com>
2021-09-23 09:08:20 +0300

    mergeSort was tested and realised

--- 2lab/taskA/taskA.cpp
+++ 2lab/taskA/taskA.cpp
@@ -3,51 +3,48 @@
 
 using namespace std;
 
-void merge(int a[], int Firstindex, int m, int Lastindex); //merges the sub-arrays which are created while division
-
-void mergeSort(int a[], int Firstindex, int Lastindex) {
-    if (Firstindex < Lastindex) {
-        int m = Firstindex + (Lastindex - Firstindex) / 2;
-        mergeSort(a, Firstindex, m);
-        mergeSort(a, m + 1, Lastindex);
-        merge(a, Firstindex, m, Lastindex);
-    }
-}
-
-void merge(int a[], int Firstindex, int m, int Lastindex) {
-    int x;
-    int y;
-    int z;
-    int sub1 = m - Firstindex + 1;
-    int sub2 = Lastindex - m;
-    int First[sub1];  //temp array
-    int Second[sub2];
-    for (x = 0; x < sub1; x++) // copying data to temp arrays
-        First[x] = a[Firstindex + x];
+void merge(int array[], int firstIndex, int m, int lastIndex) {
+    int x,y,z;
+    int sub1 = m - firstIndex + 1;
+    int sub2 = lastIndex - m;
+    int first[sub1];
+    int second[sub2];
+    for (x = 0; x < sub1; x++)
+        first[x] = array[firstIndex + x];
     for (y = 0; y < sub2; y++)
-        Second[y] = a[m + 1 + y];
+        second[y] = array[m + 1 + y];
+
     x = 0;
     y = 0;
-    z = Firstindex;
+    z = firstIndex;
     while (x < sub1 && y < sub2) {
-        if (First[x] <= Second[y]) {
-            a[z] = First[x];
+        if (first[x] <= second[y]) {
+            array[z] = first[x];
             x++;
         } else {
-            a[z] = Second[y];
+            array[z] = second[y];
             y++;
         }
         z++;
     }
     while (x < sub1) {
-        a[z] = First[x];
+        array[z] = first[x];
         x++;
         z++;
     }
     while (y < sub2) {
-        a[z] = Second[y];
+        array[z] = second[y];
         y++;
         z++;
+    }
+}
+
+void mergeSort(int array[], int firstIndex, int lastIndex) {
+    if (firstIndex < lastIndex) {
+        int middleIndex = firstIndex + (lastIndex - firstIndex) / 2;
+        mergeSort(array, firstIndex, middleIndex);
+        mergeSort(array, middleIndex + 1, lastIndex);
+        merge(array, firstIndex, middleIndex, lastIndex);
     }
 }
 
@@ -55,14 +52,11 @@ int main() {
     int amount;
     ifstream fileIn("sort.in");
     ofstream fileOut("sort.out");
-
     fileIn >> amount;
-
     int *arr = new int[amount];
     for (int i = 0; i < amount; i++) {
         fileIn >> arr[i];
     }
-    // algo itself
     mergeSort(arr, 0, amount - 1);
     for (int i = 0; i < amount; i++) {
         fileOut << arr[i] << " ";
