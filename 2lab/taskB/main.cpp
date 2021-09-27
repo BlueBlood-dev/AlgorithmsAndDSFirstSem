ccdb8bc2554632af3f6e5017077cb3ec7d92ffd6
Denis Khashchuk <khashchuk.denis@gmail.com>
2021-09-27 09:05:46 +0300

    .

--- 2lab/taskB/main.cpp
+++ 2lab/taskB/main.cpp
@@ -3,16 +3,23 @@
 
 using namespace std;
 
-void merge(string array[], int firstIndex, int m, int lastIndex) {
+void merge(string array[],string s[], int firstIndex, int m, int lastIndex) {
     int x, y, z;
     int firstHalfIndex = m - firstIndex + 1;
     int secondHalfIndex = lastIndex - m;
     string firstHalf[firstHalfIndex];
     string secondHalf[secondHalfIndex];
-    for (x = 0; x < firstHalfIndex; x++)
+    string firstHalf_surname[firstHalfIndex];
+    string secondHalf_surname[secondHalfIndex];
+    for (x = 0; x < firstHalfIndex; x++) {
         firstHalf[x] = array[firstIndex + x];
-    for (y = 0; y < secondHalfIndex; y++)
+        firstHalf_surname[x] = s[firstIndex + x];
+    }
+
+    for (y = 0; y < secondHalfIndex; y++) {
         secondHalf[y] = array[m + 1 + y];
+        secondHalf_surname[y] = s[m+1+y];
+    }
 
     x = 0;
     y = 0;
@@ -21,40 +28,36 @@ void merge(string array[], int firstIndex, int m, int lastIndex) {
     while (x < firstHalfIndex && y < secondHalfIndex) {
         if (firstHalf[x] <= secondHalf[y]) {
             array[z] = firstHalf[x];
+            s[z] = firstHalf_surname[x];
             x++;
         } else {
             array[z] = secondHalf[y];
+            s[z] = secondHalf_surname[y];
             y++;
         }
         z++;
     }
     while (x < firstHalfIndex) {
         array[z] = firstHalf[x];
+        s[z] = firstHalf_surname[x];
         x++;
         z++;
     }
     while (y < secondHalfIndex) {
         array[z] = secondHalf[y];
+        s[z] = secondHalf_surname[y];
         y++;
         z++;
     }
 }
 
-void mergeSort(string array[], int firstIndex, int lastIndex) {
+void mergeSort(string array[], string s[],int firstIndex, int lastIndex) {
     if (firstIndex < lastIndex) {
         int middleIndex = firstIndex + (lastIndex - firstIndex) / 2;
-        mergeSort(array, firstIndex, middleIndex);
-        mergeSort(array, middleIndex + 1, lastIndex);
-        merge(array, firstIndex, middleIndex, lastIndex);
+        mergeSort(array,s, firstIndex, middleIndex);
+        mergeSort(array,s, middleIndex + 1, lastIndex);
+        merge(array,s, firstIndex, middleIndex, lastIndex);
     }
-}
-
-
-int findIfInArray(string temp[], int size, string basicString) {
-    for (int i = 0; i < size; i++)
-        if (temp[i] == basicString)
-            return i;
-    return -1;
 }
 
 int main() {
@@ -70,20 +73,20 @@ int main() {
         fileIn >> country[i] >> cash_surname[i];
         cash_country[i] = country[i];
     }
-    mergeSort(country, 0, amount - 1);
-    string tempArray[amount];
-    for (int i = 0; i < amount; i++) {
-        if (findIfInArray(tempArray, amount, country[i]) == -1) {
+    mergeSort(country,cash_surname, 0, amount - 1);
+    fileOut << "=== " << country[0] << " ===\n";
+    fileOut << cash_surname[0] << "\n";
+    for (int i = 1; i < amount; i++) {
+        if (country[i] != country[i - 1])
             fileOut << "=== " << country[i] << " ===\n";
-            tempArray[i] = country[i];
-            for (int j = 0; j < amount; j++) {
-                if (cash_country[j] == country[i]) {
-                    fileOut << cash_surname[j] << '\n';
-                }
-            }
+        fileOut << cash_surname[i] << "\n";
+
         }
-    }
 
     fileIn.close();
     fileOut.close();
 }
+
+
+
+
