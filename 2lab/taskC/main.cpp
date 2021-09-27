c617fcb48691b8011240a983b5d0954a660a8714
Denis Khashchuk <khashchuk.denis@gmail.com>
2021-09-27 09:13:29 +0300

    3 task was started

---
+++ 2lab/taskC/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+int main() {
+    int array[] = {1,2,3,4};
+    for (int number : array) {
+        cout << number << "\n";
+    }
+}
