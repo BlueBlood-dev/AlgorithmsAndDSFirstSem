0c7c1a2ae6e2ce27c103a2f5d8aea18e6a7b75fe
Denis Khashchuk <khashchuk.denis@gmail.com>
2021-09-28 19:37:17 +0300

    antiqtask was finished

--- 2lab/taskD/main.cpp
+++ 2lab/taskD/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int amount;
+    ifstream in("antiqs.in");
+    ofstream out("antiqs.out");
+    in >> amount;
+    int array[amount];
+    for (int i = 0; i< amount; ++i)
+        array[i] = i+1;
+    for(int i = 0; i < amount; ++i)
+        swap(array[i],array[i/2]);
+    for(int i = 0; i < amount; ++i)
+        out << array[i] << " ";
+
 }
